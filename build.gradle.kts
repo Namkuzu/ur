aea325e162dbccfd520c265f4fdbd394f73b7535
amonserrat <alvaro.monserrat@gmail.com>
2023-08-30 21:55:56 -0400

    add jitpack.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,33 @@ plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.android.library") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.8.10" apply false
+}
+
+buildscript {
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.3.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
+    }
+
+    repositories {
+        google()
+        gradlePluginPortal()
+    }
+}
+
+subprojects {
+    repositories {
+        google()
+        gradlePluginPortal()
+    }
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            // Allow warnings when running from IDE, makes it easier to experiment.
+            allWarningsAsErrors = true
+
+            jvmTarget = "1.8"
+        }
+    }
 }
